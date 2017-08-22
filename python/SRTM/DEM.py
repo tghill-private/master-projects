d52d0b527851dce643d960df9ccfdab170753e2d
Tim Hill <timofhill@gmail.com>
2017-08-22 14:36:46 -0400

    Add operational code

---
+++ python/SRTM/DEM.py
@@ -0,0 +1,67 @@
+"""
+
+    module DEM.py
+
+    Opens binary '.hgt' DEM files and converts to a 2D numpy array
+
+    Download SRTM files from the ftp server
+    https://dds.cr.usgs.gov/srtm/version2_1/SRTM3/North_America/
+
+"""
+
+import os
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+file_path = "srtm_dem/{lat}{lon}.hgt"
+
+def get_dem_path(lat, lon):
+    lat_str = "N%s"%int(np.floor(lat)) if lat>=0 else "S%s"%int(abs(np.floor(lat)))
+    lon_str = "E%s"%int(np.floor(lon)) if lon>=0 else "W%.3d"%int(abs(np.floor(lon)))
+    path = file_path.format(lat=lat_str, lon=lon_str)
+    return path
+
+class DEM:
+    _samples = 1201
+    _shape = (_samples, _samples)
+    def __init__(self, lat, lon):
+        ilat, ilon = int(np.floor(lat)), int(np.floor(lon))
+        dem_file = get_dem_path(lat, lon)
+        with open(dem_file, 'r') as dem:
+            elevations = np.fromfile(dem, dtype=np.dtype('>i2')).reshape(DEM._shape)
+        lats = np.linspace(ilat+1, ilat, DEM._samples)
+        lons = np.linspace(ilon, ilon+1, DEM._samples)
+        latlon = np.meshgrid(lons, lats)
+
+        self.elevations = elevations
+        self.latlon = latlon
+        self.latitudes = lats
+        self.longitudes = lons
+
+        self.srtm_dem = os.path.split(dem_file)[1]
+
+    def __getitem__(self, coord):
+        lat, lon = coord
+        lat_row = int(np.floor((lat - int(lat)) * (DEM._samples - 1)))
+        lat_row = DEM._samples - 1 - lat_row
+        lon_col = int(np.floor( (lon - int(lon)) * (DEM._samples - 1)))
+
+        # latlon_chk = (self.latlon[0][lat_row, lon_col], self.latlon[1][lat_row, lon_col])
+        #
+        # print latlon_chk
+        return self.elevations[lat_row, lon_col]
+
+
+def main():
+    fig, ax = plt.subplots()
+
+    dem = DEM(44.51, -80.3)
+    ax.pcolormesh(dem.latlon[0], dem.latlon[1][::-1], dem.elevations[::-1])
+
+    ax.set_xlim(dem.longitudes[0], dem.longitudes[-1])
+    ax.set_ylim(dem.latitudes[-1], dem.latitudes[0])
+    fig.savefig('{}.png'.format(dem.srtm_dem), dpi=600)
+
+if __name__ == "__main__":
+    main()
