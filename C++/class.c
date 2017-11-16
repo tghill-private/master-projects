3deabfc98ac27e370d32173732a2ec6cff8cbf8e
timghill <timofhill@gmail.com>
2017-11-15 20:36:23 -0500

    Edit C++ classes

--- C++/class.c
+++ C++/class.c
@@ -1,41 +1,58 @@
+/**
+file class.cpp
+
+This file is for playing around with classes in C++
+**/
 #include <iostream>
 using namespace std;
 
 class animal {
+  public:
+    // animal (string pet_name, float pet_weight, string pet_colour): name(pet_name), weight(pet_weight), colour(colour) {};
+    animal(string pet_name, float pet_weight, string pet_colour) {
+      name = pet_name;
+      weight = pet_weight;
+      colour = pet_colour;
+    };
+
+    int get_weight () {return weight;
+    };
+    void set_weight( float new_weight ) {
+      weight = new_weight;
+    };
+
+    string get_name () {return name;
+    };
+    void set_name(string new_name) {
+      name = new_name;
+    };
+
+    string get_colour () {return colour;
+    };
+    void set_colour(string new_colour) {
+      colour = new_colour;
+    };
+
+  private:
     string name;
     int weight;
-  public:
-    animal (string, float);
     string colour;
-    void set_weight( float new_weight );
-    int get_weight () {return weight;}
-    string get_name () {return name;}
 };
-
-animal::animal (string pet_name, float pet_weight):name(pet_name), weight(pet_weight) {};
-
-void animal::set_weight( float new_weight ){
-    weight = new_weight;
-	}
 
 
 int main(){
 //    string lily="Lily";
 //    string pet = "pet";
-    animal animal1 ("lily", 20);
-    animal animal2 ("pet", 12);
+    animal animal1 ("lily", 20, "Black");
+    animal animal2 ("pet", 12, "Brown");
 
-    string c1="Black";
-    string c2 = "Brown";
-
-    animal1.colour = c1;
-    animal2.colour = c2;
-
-    cout << "Animal 1 Name, weight, colour: " << animal1.get_name() << animal1.get_weight() << animal1.colour<<"\n";
-    cout << "Animal 2 Name, weight colour:" << animal2.get_name() << animal2.get_weight() << animal2.colour <<"\n";
+    cout << "Animal 1 Name, weight, colour: " << animal1.get_name() << animal1.get_weight() << animal1.get_colour()<<"\n";
+    cout << "Animal 2 Name, weight colour:" << animal2.get_name() << animal2.get_weight() << animal2.get_colour() <<"\n";
 
     animal1.set_weight(61);
     cout <<" Animal 1 Weight: " << animal1.get_weight() << "\n";
+
+    // cout <<"Animal 2 Colour: " << animal2.colour
 
     return 0;
 }
